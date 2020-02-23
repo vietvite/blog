a4211bc4854f5b514e87a12413ec6216145b98e6
vietvite <vanviet8367@gmail.com>
2020-02-23 18:44:31 +0700

    Updates

---
+++ precache-manifest.0b82e85d10d3c408aec77b2d9a868371.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672f0a244afa77459b5edd8793ad0a36",
+    "url": "./index.html"
+  },
+  {
+    "revision": "59a745240980439d8211",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9a5259c6b01c921bbb9d",
+    "url": "./static/js/2.dcfc4e55.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.dcfc4e55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59a745240980439d8211",
+    "url": "./static/js/main.1787738c.chunk.js"
+  },
+  {
+    "revision": "ffaeb3ca47361a38a599",
+    "url": "./static/js/runtime-main.037ce7a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
